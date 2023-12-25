a273747258cf88e88ac60504550fbac585e5f493
Fynotix <dollorrblx@gmail.com>
2023-12-25 15:56:29 -0700

    global mapping & c functions

--- source/sakura.c
+++ source/sakura.c
@@ -4,6 +4,14 @@
 
 #include "assembler.h"
 
+unsigned int hash(const char *key, size_t len, size_t capacity) {
+    unsigned int hashValue = 0;
+    size_t p = 0;
+    while (p < len)
+        hashValue = (hashValue << 5) + key[p++];
+    return hashValue % capacity;
+}
+
 SakuraState *sakura_createState() {
     SakuraState *state = malloc(sizeof(SakuraState));
     if (state != NULL) {
@@ -15,12 +23,26 @@ SakuraState *sakura_createState() {
         state->pool.capacity = 8;
         state->pool.constants = malloc(state->pool.capacity * sizeof(TValue));
 
-        state->globals = malloc(128 * sizeof(int));
-        state->globalsSize = 128;
-        state->globalsIndex = 0;
         state->callStack = malloc(128 * sizeof(int));
         state->callStackSize = 128;
         state->callStackIndex = 0;
+
+        sakuraX_initializeTVMap(&state->globals, 16);
+
+        // initialize registry
+        state->registry.rax.tt = SAKURA_TNUMFLT;
+        state->registry.rax.value.n = 0;
+        state->registry.rbx.tt = SAKURA_TNUMFLT;
+        state->registry.rbx.value.n = 0;
+        state->registry.rcx.tt = SAKURA_TNUMFLT;
+        state->registry.rcx.value.n = 0;
+        state->registry.rdx.tt = SAKURA_TNUMFLT;
+        state->registry.rdx.value.n = 0;
+
+        for (size_t i = 0; i < 64; i++) {
+            state->registry.args[i].tt = SAKURA_TNUMFLT;
+            state->registry.args[i].value.n = 0;
+        }
     }
 
     return state;
@@ -28,19 +50,66 @@ SakuraState *sakura_createState() {
 
 void sakura_destroyState(SakuraState *state) {
     if (state != NULL) {
+        sakuraX_destroyTVMap(&state->globals);
         free(state->pool.constants);
         state->pool.constants = NULL;
         state->pool.size = 0;
-        free(state->globals);
-        state->globals = NULL;
-        state->globalsSize = 0;
-        state->globalsIndex = 0;
         free(state->callStack);
         state->callStack = NULL;
         state->callStackSize = 0;
         state->callStackIndex = 0;
         free(state);
     }
+}
+
+void sakuraX_initializeTVMap(struct TVMap *map, size_t initCapacity) {
+    map->pairs = malloc(initCapacity * sizeof(struct TVMapPair));
+    map->size = 0;
+    map->capacity = initCapacity;
+
+    for (size_t i = 0; i < initCapacity; i++) {
+        map->pairs[i].init = 0;
+    }
+}
+
+void sakuraX_resizeTVMap(struct TVMap *map, size_t newCapacity) {
+    map->pairs = realloc(map->pairs, newCapacity * sizeof(struct TVMapPair));
+    map->capacity = newCapacity;
+}
+
+void sakuraX_TVMapInsert(struct TVMap *map, const struct s_str *key, TValue value) {
+    if ((double)map->size / map->capacity >= 0.75) {
+        sakuraX_resizeTVMap(map, map->capacity * 2);
+    }
+
+    size_t idx = hash(key->str, key->len, map->capacity);
+    map->pairs[idx].key = s_str_copy(key);
+    map->pairs[idx].value = value;
+    map->pairs[idx].init = 1;
+    map->size++;
+}
+
+TValue *sakuraX_TVMapGet(struct TVMap *map, const struct s_str *key) {
+    size_t idx = hash(key->str, key->len, map->capacity);
+    return map->pairs[idx].init == 1 ? &map->pairs[idx].value : NULL;
+}
+
+TValue *sakuraX_TVMapGet_c(struct TVMap *map, const char *key) {
+    size_t idx = hash(key, strlen(key), map->capacity);
+    return map->pairs[idx].init == 1 ? &map->pairs[idx].value : NULL;
+}
+
+void sakuraX_destroyTVMap(struct TVMap *map) {
+    for (size_t i = 0; i < map->size; i++) {
+        s_str_free(&map->pairs[i].key);
+        if (map->pairs[i].value.tt == SAKURA_TSTR) {
+            s_str_free(&map->pairs[i].value.value.s);
+        }
+    }
+    free(map->pairs);
+    map->pairs = NULL;
+    map->size = 0;
+    map->capacity = 0;
 }
 
 TValue sakuraY_makeTNumber(double value) {
@@ -57,52 +126,76 @@ TValue sakuraY_makeTString(struct s_str *value) {
     return val;
 }
 
-// void sakuraY_push(SakuraState *S, TValue val) {
-//     if (S->stackIndex >= SAKURA_STACK_SIZE) {
-//         printf("Error: stack overflow\n");
-//         exit(1);
-//     }
-//     S->stack[S->stackIndex++] = val;
-// }
+TValue sakuraY_makeTCFunc(int (*fnPtr)(SakuraState *)) {
+    TValue val;
+    val.tt = SAKURA_TCFUNC;
+    val.value.cfn = fnPtr;
+    return val;
+}
 
-// TValue sakuraY_pop(SakuraState *S) {
-//     if (S->stackIndex <= 0) {
-//         printf("Error: stack underflow\n");
-//         exit(1);
-//     }
-//     return S->stack[--S->stackIndex];
-// }
-
-TValue sakuraY_getStack(SakuraState *S, int index) {
-    if (index < 0) {
-        int tidx = -index - 1;
-        if (tidx >= S->pool.size) {
-            printf("Error: constant index out of bounds\n");
-            exit(1);
-        }
-        return S->pool.constants[tidx];
+void sakura_setGlobal(SakuraState *S, const struct s_str *name) {
+    TValue *val = sakuraX_TVMapGet(&S->globals, name);
+    if (val == NULL) {
+        sakuraX_TVMapInsert(&S->globals, name, sakuraY_pop(S));
+    } else {
+        *val = sakuraY_pop(S);
     }
+}
 
-    if (index >= SAKURA_STACK_SIZE) {
-        printf("Error: stack index out of bounds\n");
+void sakuraY_push(SakuraState *S, TValue val) {
+    if (S->stackIndex >= SAKURA_STACK_SIZE) {
+        printf("Error: stack overflow\n");
+        exit(1);
+    }
+    S->stack[S->stackIndex++] = val;
+}
+
+TValue sakuraY_pop(SakuraState *S) {
+    if (S->stackIndex <= 0) {
+        printf("Error: stack underflow\n");
+        exit(1);
+    }
+    return S->stack[--S->stackIndex];
+}
+
+TValue *sakuraY_peak(SakuraState *S) {
+    if (S->stackIndex <= 0) {
+        printf("Error: stack underflow\n");
         exit(1);
     }
 
-    return S->stack[index];
+    return &(S->stack[S->stackIndex - 1]);
 }
 
-// TValue *sakuraY_peak(SakuraState *S) {
-//     if (S->stackIndex <= 0) {
-//         printf("Error: stack underflow\n");
-//         exit(1);
-//     }
+int sakura_peak(SakuraState *S) {
+    if (S->stackIndex <= 0) {
+        printf("Error: stack underflow\n");
+        exit(1);
+    }
 
-//     return &(S->stack[S->stackIndex - 1]);
-// }
+    return (int)(S->stack[S->stackIndex - 1].value.n);
+}
 
-// int sakuraL_isNumber(SakuraState *S) { return sakuraY_peak(S)->tt == SAKURA_TNUMFLT; }
+int sakura_isNumber(SakuraState *S) { return sakuraY_peak(S)->tt == SAKURA_TNUMFLT; }
+int sakura_isString(SakuraState *S) { return sakuraY_peak(S)->tt == SAKURA_TSTR; }
 
-// int sakuraL_isString(SakuraState *S) { return sakuraY_peak(S)->tt == SAKURA_TSTR; }
+double sakura_popNumber(SakuraState *S) {
+    TValue val = sakuraY_pop(S);
+    if (val.tt != SAKURA_TNUMFLT) {
+        printf("Error: expected number, got %d\n", val.tt);
+        exit(1);
+    }
+    return val.value.n;
+}
+
+struct s_str sakura_popString(SakuraState *S) {
+    TValue val = sakuraY_pop(S);
+    if (val.tt != SAKURA_TSTR) {
+        printf("Error: expected string, got %d\n", val.tt);
+        exit(1);
+    }
+    return val.value.s;
+}
 
 void copyTValue(TValue *dest, TValue *src) {
     dest->tt = src->tt;
--- source/sakura.h
+++ source/sakura.h
@@ -20,14 +20,20 @@ typedef unsigned short SakuraFlag;
 #define SAKURA_EFLAG_RUNTIME 2
 #define SAKURA_EFLAG_FATAL 3
 
-union SakuraValue {
-    double n;       // TNUMFLT
-    struct s_str s; // TSTR
-};
-
 // #define SAKURA_TNUMINT 1 // integer tag
 #define SAKURA_TNUMFLT 0 // float tag
 #define SAKURA_TSTR 2    // string tag
+#define SAKURA_TCFUNC 3  // C function tag
+#define SAKURA_TNIL 4    // nil tag
+
+struct SakuraState;
+
+union SakuraValue {
+    double n;                         // TNUMFLT
+    struct s_str s;                   // TSTR
+    int (*cfn)(struct SakuraState *); // TCFUNC
+    int nil;                          // TNIL
+};
 
 // TValue represents a tagged value
 typedef struct {
@@ -41,13 +47,33 @@ typedef struct {
     size_t capacity;
 } SakuraConstantPool;
 
+typedef struct {
+    TValue rax;
+    TValue rbx;
+    TValue rcx;
+    TValue rdx;
+
+    TValue args[64];
+} SakuraRegistry;
+
+struct TVMapPair {
+    struct s_str key;
+    TValue value;
+    int init;
+};
+
+struct TVMap {
+    struct TVMapPair *pairs;
+    size_t size;
+    size_t capacity;
+};
+
 struct SakuraState {
     TValue stack[SAKURA_STACK_SIZE];
     int stackIndex;
+    SakuraRegistry registry;
     SakuraConstantPool pool;
-    int *globals;
-    size_t globalsSize;
-    size_t globalsIndex;
+    struct TVMap globals;
     int *callStack;
     size_t callStackSize;
     size_t callStackIndex;
@@ -61,8 +87,12 @@ typedef struct SakuraState SakuraState;
 SakuraState *sakura_createState();
 void sakura_destroyState(SakuraState *state);
 
-// #define SAKURA_TAG_NUMBER 0
-// #define SAKURA_TAG_STRING 1
+void sakuraX_initializeTVMap(struct TVMap *map, size_t initCapacity);
+void sakuraX_resizeTVMap(struct TVMap *map, size_t newCapacity);
+void sakuraX_TVMapInsert(struct TVMap *map, const struct s_str *key, TValue value);
+TValue *sakuraX_TVMapGet(struct TVMap *map, const struct s_str *key);
+TValue *sakuraX_TVMapGet_c(struct TVMap *map, const char *key);
+void sakuraX_destroyTVMap(struct TVMap *map);
 
 #define SET_TAG(value, tag) ((int)((value) | ((tag) << 29)))
 #define GET_TAG(value) ((value) >> 29)
@@ -72,11 +102,17 @@ void sakuraY_mergePools(SakuraState *S, SakuraConstantPool *pool);
 
 TValue sakuraY_makeTNumber(double value);
 TValue sakuraY_makeTString(struct s_str *value);
+TValue sakuraY_makeTCFunc(int (*fnPtr)(SakuraState *));
 
-// void sakuraY_push(SakuraState *S, TValue val);
-// TValue sakuraY_pop(SakuraState *S);
-TValue sakuraY_getStack(SakuraState *S, int index);
-// TValue *sakuraY_peak(SakuraState *S);
+void sakura_setGlobal(SakuraState *S, const struct s_str *name);
 
-// int sakuraL_isNumber(SakuraState *S);
-// int sakuraL_isString(SakuraState *S);
+void sakuraY_push(SakuraState *S, TValue val);
+TValue sakuraY_pop(SakuraState *S);
+TValue *sakuraY_peak(SakuraState *S);
+int sakura_peak(SakuraState *S);
+
+int sakura_isNumber(SakuraState *S);
+int sakura_isString(SakuraState *S);
+
+double sakura_popNumber(SakuraState *S);
+struct s_str sakura_popString(SakuraState *S);
