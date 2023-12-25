ba49ac0132ac1a079e9218581a7e2b4fc1b9b665
Fynotix <dollorrblx@gmail.com>
2023-12-25 15:56:49 -0700

    sakura_register, loadStdlib functions

--- source/sap.c
+++ source/sap.c
@@ -6,6 +6,27 @@
 #include "parser.h"
 #include "svm.h"
 
+int sakuraS_print(SakuraState *S) {
+    int args = sakura_peak(S);
+    sakuraY_pop(S);
+
+    for (int i = 0; i < args; i++) {
+        if (sakura_isNumber(S)) {
+            printf("%f    ", sakura_popNumber(S));
+        } else if (sakura_isString(S)) {
+            struct s_str val = sakura_popString(S);
+            printf("%.*s    ", val.str, val.len);
+        } else {
+            printf("?    ");
+        }
+    }
+
+    printf("\n");
+    return 0;
+}
+
+void sakuraL_loadStdlib(SakuraState *S) { sakura_register(S, "print", sakuraS_print); }
+
 void sakuraL_loadfile(SakuraState *S, const char *file) {
     struct s_str source = readfile(file);
     if (source.str == NULL) {
@@ -23,20 +44,12 @@ void sakuraL_loadstring(SakuraState *S, struct s_str *source) {
     struct NodeStack *nodes = sakuraY_parse(S, tokens);
     struct SakuraAssembly *assembly = sakuraY_assemble(S, nodes);
 
+    sakuraL_loadStdlib(S);
+
     // sakuraX_writeDisasm(assembly, "test.sa");
     sakuraX_interpret(S, assembly);
 
-    for (size_t i = 0; i < S->stackIndex; i++) {
-        TValue val = S->stack[i];
-        switch (val.tt) {
-        case SAKURA_TNUMFLT:
-            printf("%f\n", val.value.n);
-            break;
-        case SAKURA_TSTR:
-            printf("%.*s\n", val.value.s.len, val.value.s.str);
-            break;
-        }
-    }
+    TValue *val = sakuraX_TVMapGet_c(&S->globals, "print");
 
     sakuraX_freeAssembly(assembly);
     sakuraX_freeNodeStack(nodes);
@@ -47,4 +60,12 @@ void sakuraL_loadstring_c(SakuraState *S, const char *str) {
     struct s_str source = s_str(str);
     sakuraL_loadstring(S, &source);
     s_str_free(&source);
+}
+
+void sakuraL_registerGlobalFn(SakuraState *S, const char *name, int (*fnPtr)(SakuraState *)) {
+    struct s_str nameStr = s_str(name);
+    TValue val = sakuraY_makeTCFunc(fnPtr);
+    sakuraY_push(S, val);
+    sakura_setGlobal(S, &nameStr);
+    s_str_free(&nameStr);
 }
--- source/sap.h
+++ source/sap.h
@@ -2,6 +2,12 @@
 
 #include "sakura.h"
 
+#define sakura_register(S, name, ptr) sakuraL_registerGlobalFn(S, name, ptr)
+
+void sakuraL_loadStdlib(SakuraState *S);
+
 void sakuraL_loadfile(SakuraState *S, const char *file);
 void sakuraL_loadstring(SakuraState *S, struct s_str *source);
 void sakuraL_loadstring_c(SakuraState *S, const char *source);
+
+void sakuraL_registerGlobalFn(SakuraState *S, const char *name, int (*fnPtr)(SakuraState *));
